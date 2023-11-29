323a4c7777a7b18a961a1b5ca8189988457d1e64
chrlie-w <chrlie.w@proton.me>
2023-11-29 20:11:53 +0000

    CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,20 @@ const path = require('path')
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: "main.js"
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        assetModuleFilename: 'images/[hash][ext]',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\,(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ],
     },
 };
