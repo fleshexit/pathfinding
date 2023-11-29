2b11c7b68c99dfad6eec2b3f8583e2638071913e
chrlie-w <chrlie.w@proton.me>
2023-11-29 20:13:40 +0000

    Event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import { loadPage } from "./load-page.js";
+import { populateGrid, getColour, setColour} from "./create-grid.js";
+import "./style.css";
 
-loadPage();
+function changeSize(size){
+    populateGrid(size);
+}
+
+function clearGrid() {
+    let cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => cell.style.backgroundColor = "white")
+}
+
+const clearButton = document.querySelector("#clear-btn");
+clearButton.addEventListener("click", clearGrid);
+
+const sizeSlider = document.querySelector("#size-slider");
+sizeSlider.addEventListener("change", () => changeSize(sizeSlider.value));
+
+const blockButton = document.querySelector("#block-btn");
+blockButton.addEventListener("click", () => setColour("black"));
+
+const eraserButton = document.querySelector("#eraser-btn");
+eraserButton.addEventListener("click", () => setColour("white"));
+
+populateGrid(16);
