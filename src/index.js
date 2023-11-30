c526a092c87210ff99744cd6c84f026a9e9c96c7
chrlie-w <chrlie.w@proton.me>
2023-11-30 00:55:38 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -15,17 +15,22 @@ function clearGrid() {
 const clearButton = document.querySelector("#clear-btn");
 clearButton.addEventListener("click", clearGrid);
 
-const sizeSlider = document.querySelector("#size-slider");
-sizeSlider.addEventListener("change", () => changeSize(sizeSlider.value));
-
 const blockButton = document.querySelector("#block-btn");
 blockButton.addEventListener("click", () => setColour("black"));
 
 const eraserButton = document.querySelector("#eraser-btn");
 eraserButton.addEventListener("click", () => setColour("white"));
 
+
+const endButton = document.querySelector("#end-btn");
+endButton.addEventListener("click", () => setColour("red"));
+
 const menu = document.querySelector(".menu");
 const menuButton = document.querySelector("#menu-btn");
 menuButton.addEventListener("click", () => menu.showModal());
+
+
+const sizeSlider = document.querySelector("#size-slider");
+sizeSlider.addEventListener("change", () => changeSize(sizeSlider.value));
 
 populateGrid(16);
